60f762f9a42241a643ca51e2db68b72f75994d5f
Rifat Hossain <hossainrifat221@gmail.com>
2025-08-01 23:32:16 +0600

    Updated EmployeeController with H2 DB logic

--- src/main/java/com/example/SpringBootDemo/controller/EmployeeV2Controller.java
+++ src/main/java/com/example/SpringBootDemo/controller/EmployeeV2Controller.java
@@ -1,17 +1,42 @@
 package com.example.SpringBootDemo.controller;
 
 import com.example.SpringBootDemo.model.Employee;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.SpringBootDemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v2/employees")
 public class EmployeeV2Controller {
+
+
+    @Qualifier("employeeV2ServiceImpl")
+    @Autowired
+    EmployeeService employeeService;
+
     @PostMapping
     public Employee save(@RequestBody Employee employee)
     {
-        return employee;
+        return employeeService.save(employee);
     }
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+    @GetMapping("/{Id}")
+    public Employee getEmployeeById(@PathVariable String Id)
+    {
+        return employeeService.getEmployeeById(Id);
+    }
+    @DeleteMapping("/{Id}")
+    public String deleteEmployeeById(@PathVariable String Id)
+    {
+        return employeeService.deleteEmployeeById(Id);
+    }
+
+
 }
--- src/main/java/com/example/SpringBootDemo/service/EmployeeV2ServiceImpl.java
+++ src/main/java/com/example/SpringBootDemo/service/EmployeeV2ServiceImpl.java
@@ -1,30 +1,62 @@
 package com.example.SpringBootDemo.service;
 
+import com.example.SpringBootDemo.entity.EmployeeEntity;
+import com.example.SpringBootDemo.error.EmployeeNotFoundException;
 import com.example.SpringBootDemo.model.Employee;
+import com.example.SpringBootDemo.repository.EmployeeRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeV2ServiceImpl implements EmployeeService{
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Override
     public Employee save(Employee employee) {
-        return null;
+        if(employee.getEmployeeId()==null || employee.getEmailId().isEmpty())
+        {
+            employee.setEmployeeId(UUID.randomUUID().toString());
+
+        }
+
+        EmployeeEntity entity = new EmployeeEntity();
+        BeanUtils.copyProperties(employee,entity);
+        employeeRepository.save(entity);
+        return employee;
+
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        return List.of();
+         List<EmployeeEntity> employeeEntities=employeeRepository.findAll();
+         List<Employee>employees=employeeEntities.
+                 stream()
+                 .map(employeeEntity -> {
+                     Employee employee = new Employee();
+                     BeanUtils.copyProperties(employeeEntity,employee);
+                     return employee;
+                 }).collect(Collectors.toList());
+            return employees;
     }
 
     @Override
     public Employee getEmployeeById(String Id) {
-        return null;
+        EmployeeEntity employeeEntity=employeeRepository.findById(Id).get();
+        Employee employee=new Employee();
+        BeanUtils.copyProperties(employeeEntity,employee);
+        return employee;
     }
 
     @Override
     public String deleteEmployeeById(String id) {
-        return "";
+        employeeRepository.deleteById(id);
+        return "Employee deleted with id: "+id;
     }
 }
