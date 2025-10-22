569e9c4888eaaa6a91aa4ad94466a2233e6924f7
Rifat Hossain <hossainrifat221@gmail.com>
2025-10-22 20:17:51 +0600

    updated

--- OrderService/src/main/java/com/example/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/example/OrderService/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.example.OrderService.external.client;
 
+import com.example.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name="external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
     @PutMapping("/reduceQuantity/{id}")
@@ -13,4 +16,8 @@ public interface ProductService {
             @PathVariable("id") long productId,
             @RequestParam long quantity
     );
+
+    default void fallback(Exception e){
+        throw new CustomException("Product service is not Available!","UNAVAILABLE",500);
+    }
 }
