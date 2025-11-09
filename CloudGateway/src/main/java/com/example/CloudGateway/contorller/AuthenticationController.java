ea4f580d0fa4a9b20b594fd84e8db1ef1bb668ed
Rifat Hossain <hossainrifat221@gmail.com>
2025-11-10 00:41:42 +0600

    Updated

---
+++ CloudGateway/src/main/java/com/example/CloudGateway/contorller/AuthenticationController.java
@@ -0,0 +1,4 @@
+package com.example.CloudGateway.contorller;
+
+public class AuthenticationController {
+}
---
+++ CloudGateway/src/main/java/com/example/CloudGateway/model/AuthenticationResponse.java
@@ -0,0 +1,4 @@
+package com.example.CloudGateway.model;
+
+public class AuthenticationResponse {
+}
